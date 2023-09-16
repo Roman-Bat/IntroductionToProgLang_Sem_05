35ab87f5246c6350c6a0afbaa6f9ea03c3b87a72
Roman_Batrakov <kyct69@yandex.ru>
2023-09-16 11:35:47 +0300

    Решили первую задачу

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -5,3 +5,47 @@
 положительных чисел равна 29, сумма отрицательных равна
 -20.
 */
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+int[] Getarray(int length, int min, int max)
+{
+    int[] result = new int[length];
+    Random rand = new Random();
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = rand.Next(min, max + 1);
+        }
+        return result;
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+{
+    Console.Write(arr[i]+ " ");
+}
+
+}
+void GetPosNegSum(int[] array)
+{
+    int PositiveSum = 0;
+    int NegativeSum = 0;
+    foreach (int element in array)
+        {
+            if (element > 0) PositiveSum += element;
+            else NegativeSum += element;
+        }
+        Console.WriteLine($"Сумма положительных элементов массива {PositiveSum}, сумма отрицательных элементов массива {NegativeSum}");
+}
+int size = GetNumber("Введите длину массива");
+int min = GetNumber("Введите минимальный диапазон для массива");
+int max = GetNumber("Введите максимальный диапазон для массива");
+int [] array = Getarray(size, min, max);
+PrintArray(array);
+GetPosNegSum(array);
+
+
+
